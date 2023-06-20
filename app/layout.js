a63bee81434d659e496622bb4120669d7727894b
Bonnolo <fbonetti00@gmail.com>
2023-06-20 12:20:14 +0200

    added nextPWA

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
+//font-family: 'Inter', sans-serif;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,12 +13,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="it" data-theme="forest" className="flex justify-center">
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <link rel="icon" href="./favicon.ico" sizes="any" />
-      <body className="w-full bg-base-100">
-        {children}
-        {/* <Footer /> */}
-      </body>
+      <Head>
+        <meta name="application-name" content="TopSpeed" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="TopSpeed" />
+        <meta name="description" content="Get your speed on!" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="TopSpeed" />
+        <meta property="og:description" content="Get your speed on!" />
+        <meta property="og:site_name" content="TopSpeed" />
+        <meta property="og:url" content="https://topspeedapp.com" />
+        <meta
+          property="og:image"
+          content="https://topspeedapp.com/images/icons/icon-512x512.png"
+        />
+        <link rel="icon" href="./favicon.ico" sizes="any" />
+      </Head>
+      <body className="w-full bg-base-100">{children}</body>
     </html>
   );
 }
